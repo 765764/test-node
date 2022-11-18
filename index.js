bb6df3345974b6933ad3197b57a0a1721d814ecf
765764 <113718072+765764@users.noreply.github.com>
2022-11-18 13:50:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,40 +2,40 @@ const http = require('http');
 const fs = require('fs');
 const args = require('minimist')(process.argv.slice(2));
 
-let homepage;
-let projectpage;
-let registrationpage;
+let homePage;
+let projectPage;
+let registrationPage;
 
-fs.readfile("home.html",(err,data) => {
-	if (err) throw err;
-	homepage = data.toString();
+fs.readFile("home.html", (err, data) => {
+    if (err) throw err;
+    homePage = data.toString();
 })
 
-fs.readfile("project.html",(err,data) => {
-	if (err) throw err;
-	projectpage=data.tostring();
+fs.readFile("project.html", (err, data) => {
+    if (err) throw err;
+    projectPage = data.toString();
 })
 
-fs.readfile("registration.html",(err,data) => {
-	if (err) throw err;
-	registrationpage=data.tostring();
+fs.readFile("registration.html", (err, data) => {
+    if (err) throw err;
+    registrationPage = data.toString();
 })
 
-http.createserver((request, reponse) => {
-	let url = request.url;
-	response.writeHead(200,{'content-Type':'text/html'});
-	switch (url) {
-		case "/project":
-			response.write(projectPage);
-			response.end();
-			break;
-		case "/registration":
-			response.write(registrationPage);
-			response.end();
-			break;
-		default:
-			response.write(homePage);
-			response.end();
-			break;
-		}
+http.createServer((request, response) => {
+    let url = request.url;
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    switch (url) {
+        case "/project":
+            response.write(projectPage);
+            response.end();
+            break;
+        case "/registration":
+            response.write(registrationPage);
+            response.end();
+            break;
+        default:
+            response.write(homePage);
+            response.end();
+            break;
+    }
 }).listen(args.port);
